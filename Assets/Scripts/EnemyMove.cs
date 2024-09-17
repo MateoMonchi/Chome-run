87952289015449ec1f6c269304a25378f57ef5fe
MateoMonchi <monchiettimateo1@gmail.com>
2024-09-17 15:52:08 -0300

    fead:Final

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -5,13 +5,22 @@ using UnityEngine;
 public class EnemyMove : MonoBehaviour
 {
     public float speed;
+
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-        transform.Translate(Vector2.left*Time.deltaTime*speed);        
+        transform.Translate(Vector2.left * Time.deltaTime * speed);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Finish")
+        {
+            gameObject.SetActive(false);
+        }
     }
 }
